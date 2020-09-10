25467091fce3658a9a0f5c4b362355d5f8cd39b3
Bhardwaj_75 <diwakarb347@gmail.com>
2020-09-10 08:13:05 +0530

    Update linkedListReverse.c

--- linkedListReverse.c
+++ linkedListReverse.c
@@ -54,3 +54,31 @@ public:
         
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode*  rev(ListNode* curr, ListNode* prev){
+        
+        if(!curr){
+            return prev;
+        }
+        ListNode* tmp = curr->next;
+        curr->next=prev;
+        return rev(tmp, curr);
+        
+    }
+    ListNode* reverseList(ListNode* head) {
+       
+        return rev(head, nullptr);
+    }
+};
