d7324a6c3019b8793c2af4a8b05933e3b9f1bcb2
Bhardwaj_75 <diwakarb347@gmail.com>
2022-03-25 13:11:43 +0530

    Create unionFindAlgoPathCompression.cpp

---
+++ unionFindAlgoPathCompression.cpp
@@ -0,0 +1,52 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);//path compressoion storing root to all the parent
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            root[rootY] = rootX;
+        }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+private:
+    vector<int> root;
+};
+
+// Test Case
+int main() {
+    // for displaying booleans as literal strings, instead of 0 and 1
+    cout << boolalpha;
+    UnionFind uf(10);
+    // 1-2-5-6-7 3-8-9 4
+    uf.unionSet(1, 2);
+    uf.unionSet(2, 5);
+    uf.unionSet(5, 6);
+    uf.unionSet(6, 7);
+    uf.unionSet(3, 8);
+    uf.unionSet(8, 9);
+    cout << uf.connected(1, 5) << endl;  // true
+    cout << uf.connected(5, 7) << endl;  // true
+    cout << uf.connected(4, 9) << endl;  // false
+    // 1-2-5-6-7 3-8-9-4
+    uf.unionSet(9, 4);
+    cout << uf.connected(4, 9) << endl;  // true
+
+    return 0;
+}
